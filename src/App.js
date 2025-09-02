59a06abd3b0fa7de8600d571c2d4d5f17d0f7c81
parulsh239 <66636856+parulsh239@users.noreply.github.com>
2025-09-02 09:32:12 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import './App.css';
+import RealTimeStockAnalyzer from './components/RealTimeStockAnalyzer';
+
+function App() {
+  return (
+    <div className="App">
+      <RealTimeStockAnalyzer />
+    </div>
+  );
+}
+
+export default App;
